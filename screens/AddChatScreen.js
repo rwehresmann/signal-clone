461c08e24c7d0e51cb693d9f55a5694ee6df9cf0
Rodrigo W. Ehresmann <igoehresmann@gmail.com>
2021-02-18 11:10:06 -0300

    Disable "create chat" button if nothing was typed

--- screens/AddChatScreen.js
+++ screens/AddChatScreen.js
@@ -34,7 +34,11 @@ const AddChat = ({ navigation }) => {
           <Icon name="wechat" type="antdesign" size={24} color="black"/>
         }
       />
-      <Button onPress={createChat} title="Create new chat"/>
+      <Button 
+        disabled={!input} 
+        onPress={createChat} 
+        title="Create new chat"
+      />
     </View>
   )
 }
